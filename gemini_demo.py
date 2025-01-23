ed6ce3a7349c2658bdbb2be5d139013a9a06ec6e
jim.an <jianyu234@gmail.com>
2025-01-23 15:10:22 -0500

    integrated polygon option to trajectory_demo.py

--- gemini_demo.py
+++ gemini_demo.py
@@ -17,30 +17,51 @@ api_key=os.environ["GEMINI_API_KEY"]
 genai.configure(api_key=api_key)
 
 #Choose a Gemini model.
-# model = genai.GenerativeModel("gemini-1.5-flash")
-model = genai.GenerativeModel(model_name="gemini-2.0-flash-exp")
+# model_name = "gemini-1.5-flash"
+model_name = "gemini-1.5-pro"
+# model_name = "learnlm-1.5-pro-experimental"
+# model_name = "gemini-2.0-flash-exp"
+# model_name = "gemini-2.0-flash-thinking-exp-01-21"
 
-image_path_1 = "./sample_images/Iribe_red_01.png"
-image_path_2 = "./sample_images/Iribe_red_02.png"
+model = genai.GenerativeModel(model_name=model_name)
+
+image_path_1 = "./sample_images/path_1.png"
+image_path_2 = "./sample_images/path_2.png"
 image_path_3 = "./sample_images/annotated_img_000253.png"
 
 sample_file_1 = PIL.Image.open(image_path_1)
 sample_file_2 = PIL.Image.open(image_path_2)
 sample_file_3 = PIL.Image.open(image_path_3)
 
-prompt = ("Forget History. Consider the following two images of two probable paths of a robot. "
+prompt = ("Forget History. Consider the following two images of two probable paths of a vehicle. "
           "Consider that all dynamic obstacles will keep moving in the same direction. "
           "Assume that both paths are of the same length. What is show in Image 1? What is show in Image 2?"
-          "The current heading error is +4 degrees(right handed coordinate frame), "
-          "and the robot is 10m away from the goal. The objective is to navigate to the goal while "
+          "The objective is to navigate to the goal while "
           "prioritizing avoiding obstacles and staying in the lane. Is there any difference between "
-          "Image 1 and Image 2 in terms of achieving the goal ?")
+          "Image 1 and Image 2 in terms of achieving the goal ?"
+          "then, please give your selection of the preferred path A, B or indeterminate in the format *Answer:* <option_key>")
 
 prompt2 = ("Forget History. You are a robot exploring an indoor environment. There are many static obstacles and "
            "dynamic obstacles such as people. you must avoid the obstacles while exploring the rest of the building."
-           "given the three colored paths, which one should you pick and why? let\'s consider this step by step")
+           "given the three colored paths A, B and C, which one should you pick and why? let\'s consider this step by step, "
+           "then, please give your selection of paths by picking one of options A, B, C in the format *Answer:* <option_key>")
 
-# response = model.generate_content([prompt, sample_file_1, sample_file_2])
-response = model.generate_content([prompt2, sample_file_3])
+prompt3 = ("""Forget History. Consider the following two images, each depicting the ego view for robot in the same scenario with blue path depicting different paths for the robot (starting from the bottom to the top). Assume all cars/pedestrians and any dynamic obstacles will continue moving in their current direction."
+            Image 1:
+            Image 2:
+            Describe what is shown in Image 1.
+            Describe what is shown in Image 2.
+            The robot has a current heading error of 0 degrees (right-handed coordinate frame) and is located 10 m away from the goal. The objective is to navigate to the goal while making sure there are no obstacles along the red path, staying within the lane, and ensuring safety. Is there any difference between the paths in Image 1 and Image 2 in terms of achieving the goal?""")
 
+
+
+print(f"this is the response using {model_name}")
+response = model.generate_content([prompt3, sample_file_1, sample_file_2])
+# response = model.generate_content([prompt2, sample_file_3])
+print(f"initial response:"
+      f"{response.text}")
+second_prompt = (f"{response}"
+                 # f"please give your selection of the preferred path A, B, C or indeterminate in the format *Answer:* <option_key>")
+                 f"please give your selection of the preferred path A, B or indeterminate in the format *Answer:* <option_key>")
+response = model.generate_content([second_prompt])
 print(response.text)
--- trajectory_demo.py
+++ trajectory_demo.py
@@ -22,21 +22,21 @@ def project_to_image(trajectory, K, R, t):
 
     # Project points into the image frame
     points_2d = K @ trajectory_cam.T
-    # print(trajectory)
-    # print(trajectory_cam)
-    # print(points_2d)
+
     points_2d /= points_2d[2]
     return points_2d[:2].T
 
-
-fx, fy = 395, 395  # Focal lengths
-cx, cy = 480, 270  # image center
-width, height = 960, 540
+# constraints
+# Camera 395 parameters (example values)
+fx, fy = 721.5 , 721.5  # Focal lengths
+cx, cy = 609.56, 172.86  # image center
+width,height = 1242, 375
 
 # Camera matrix
 K = np.array([[fx, 0, cx],
               [0, fy, cy],
-              [0, 0, 1]])
+              [0,  0,  1]])
+
 
 # Camera rotaion + translation
 
@@ -47,10 +47,10 @@ t = np.array([[0], [-0.2], [0]])  # Translation vector
 
 # Parameters
 
-vel = 1.5
-omega_list = [-0.5, 0, 1]
-dt = 0.1
-steps = 5
+vel = 25
+omega_list = [0, 0.3]
+dt = 0.01
+steps = 100
 color_list = [
     (0, 0, 255), # Red
     (0, 255, 0), # Green
@@ -60,9 +60,14 @@ color_list = [
 # image_path = "/home/jim/Downloads/VLM_images/image_000007.png"
 image_path = "/home/jim/Downloads/VLM_images/image_000253.png"
 image = cv2.imread(image_path)
-plot_mode = "points"  # select lines or points
+background = image.copy()
+MODE = "poly"  # select lines, points, or poly
+alpha = 0.65 # transparency param for poly mode
 # image = cv2.resize(image, (1280,720))
 print("image shape:", image.shape)
+t0 = np.array([[-0.06], [-1.65], [0]])
+t1 = np.array([[-1.06], [-1.65], [0]])  # Translation vector (moving the COM)
+t2 = np.array([[0.94], [-1.65], [0]])  # Translation vector (moving the COM)
 
 for i, w in enumerate(omega_list):
     select_color = color_list[i % len(color_list)]
@@ -70,27 +75,46 @@ for i, w in enumerate(omega_list):
     trajectory_robot = compute_trajectory(vel, w, dt, steps)
 
     # print(f"trajectory: \n {trajectory_robot}")
-    points_2d = project_to_image(trajectory_robot, K, R, t)
+    points_2d_line = project_to_image(trajectory_robot, K, R, t0)
+    points_2d = project_to_image(trajectory_robot, K, R, t1)
+    points_2d_1 = project_to_image(trajectory_robot, K, R, t2)
+
+    points_2d_line = np.array([[width, height]]) - points_2d_line
     points_2d = np.array([[width, height]]) - points_2d
+    points_2d_1 = np.array([[width, height]]) - points_2d_1
     # Load a sample camera image
 
+    valid_points_line = []
     valid_points = []
+    valid_points_1 = []
     for point in points_2d:
         u, v = int(point[0]), int(point[1])
         if 0 <= v < image.shape[0] and 0 <= u < image.shape[1]:
             valid_points.append((u, v))
+    for point in points_2d_1:
+        u, v = int(point[0]), int(point[1])
+        if 0 <= v < image.shape[0] and 0 <= u < image.shape[1]:
+            valid_points_1.append((u, v))
+    for point in points_2d_line:
+        u, v = int(point[0]), int(point[1])
+        if 0 <= v < image.shape[0] and 0 <= u < image.shape[1]:
+            valid_points_line.append((u, v))
 
     if len(valid_points) > 1:
+        valid_points = valid_points + valid_points_1[::-1]
         valid_points = np.array(valid_points, dtype=np.int32)
+        valid_points = valid_points.reshape((-1, 1, 2))
         print(f"trajectory coordinates: {valid_points}")
 
-        if plot_mode == "lines":
+        if MODE == "lines":
             cv2.polylines(image, [valid_points], isClosed=False, color=select_color, thickness=5)  # arc
-        if plot_mode == "points":
+        if MODE == "points":
             for center in valid_points:
                 cv2.circle(image, center, radius=1, color=select_color, thickness=5)  # dots
-
-# print(points_2d)
+        if MODE == "poly":
+            cv2.fillPoly(image, [valid_points], select_color)
+            image = cv2.addWeighted(background, alpha, image, 1 - alpha, 0)
+        # print(points_2d)
 # Display the result
 cv2.imshow("window", image)
 cv2.waitKey(0)
